11f1a928abe9ed96ca4ca4c583f958278d5c2af7
Tyjow <platon.jonathan@gmail.com>
2016-08-01 12:26:46 +0200

    fix bullet off screen

--- Level1.js
+++ Level1.js
@@ -515,6 +515,7 @@ Game.Level1.prototype = {
 
 		bullet.events.onOutOfBounds.add(resetBullet, this);
 		bullet.body.allowGravity = false;
+		bullet.lifespan=1800;
 
 		// Shoot it
 		//bullet.body.velocity.x = this.BULLET_SPEED;
