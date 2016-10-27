aefe63fe088fdcf217935d42e5bf5f630ea08672
tyjow <platon.jonathan@gmail.com>
2016-10-27 22:38:53 +0200

    add menu, changed structure of dir

---
+++ states/gamemenu.js
@@ -0,0 +1,59 @@
+var GameMenu = function(game) {};
+
+GameMenu.prototype = {
+  preload: function () {
+    this.optionCount = 1;
+  },
+
+  addMenuOption: function(text, callback) {
+    var optionStyle = { font: '30pt TheMinion', fill: '#000', align: 'left', stroke: 'rgba(0,0,0,0)', srokeThickness: 4};
+    var txt = game.add.text(30, (this.optionCount * 80) + 200, text, optionStyle);
+    txt.stroke = "rgba(0,0,0,0";
+    txt.strokeThickness = 4;
+    var onOver = function (target) {
+      target.fill = "#660033";
+      target.stroke = "rgba(200,200,200,0.5)";
+      txt.useHandCursor = true;
+    };
+    var onOut = function (target) {
+      target.fill = "#000";
+      target.stroke = "rgba(0,0,0,0)";
+      txt.useHandCursor = false;
+    };
+    //txt.useHandCursor = true;
+    txt.inputEnabled = true;
+    txt.events.onInputUp.add(callback, this);
+    txt.events.onInputOver.add(onOver, this);
+    txt.events.onInputOut.add(onOut, this);
+
+    this.optionCount ++;
+
+
+  },
+
+  create: function () {
+    console.log(music);
+    if (music.name !== "music-menu" && playMusic) {
+      music.stop();
+      music = game.add.audio('music-menu');
+      music.loop = true;
+      music.play();
+    }
+    this.stage.disableVisibilityChange = true;
+    game.add.sprite(0, 0, 'menu-bg');
+    var titleStyle = { font: 'bold 60pt TheMinion', fill: '#ccccb3', align: 'center'};
+    var text = game.add.text(game.world.centerX, 100, "Tyjow RPG", titleStyle);
+    text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+    text.anchor.set(0.5);
+    this.addMenuOption('Commencer', function (e) {
+      this.game.state.start("Game");
+    });
+    this.addMenuOption('Options', function (e) {
+      this.game.state.start("Options");
+    });
+    this.addMenuOption('Credits', function (e) {
+      this.game.state.start("Credits");
+    });
+
+  }
+};
